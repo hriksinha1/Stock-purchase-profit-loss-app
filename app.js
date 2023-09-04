656bbe4321f47db32f9acc63594dc5fe3ad42e75
Hrik Sinha <dailyhrik@gmail.com>
2023-09-04 11:58:09 +0530

    initial commit

--- app.js
+++ app.js
@@ -6,16 +6,25 @@ var outputBox = document.querySelector("#output-box");
 
 function calculateProfitAndLoss (initial, quantity, current) {
 
-    if (initial>current){
+    if (initial > current){
 
-        var loss = (initial-current)*quantity;
+        var loss = (initial - current) * quantity;
         var lossPercentage = (loss/initial) * 100;
 
-        console.log ("Your loss percentage is ${loss} and the percentage is ${lossPercentage}%")
+       console.log(`Hey your loss is ${loss} and the percentage is ${lossPercentage}%`);
 
-    } else if (current>initial){
+    } else if (current > initial){
 
-    } else
+        var profit = (current-initial)*quantity;
+        var profitPercentage = (profit/initial)*100;
+
+        console.log(`Hey your profit is ${profit} and your percentage is ${profitPercentage}%`);
+
+    } else {
+
+        console.log(`No pain no gain`);
+
+    }
 
 }
 
