f8723ffd0ba49db647cbebf7dfd666ebc4ebbdb4
Hrik Sinha <dailyhrik@gmail.com>
2023-09-04 12:37:56 +0530

    initial commit

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ var stockQuantity = document.querySelector("#stock-quantity");
 var checkButton = document.querySelector("#click-btn");
 var outputBox = document.querySelector("#output-box");
 
+checkButton.addEventListener('click', clickHandler);
+
+function clickHandler() {
+
+    var ip = Number(initialPrice.value);
+    var cp = Number(currentPrice.value);
+    var sq = Number(stockQuantity.value);
+
+    calculateProfitAndLoss(ip, sq, cp);
+}
+
 function calculateProfitAndLoss (initial, quantity, current) {
 
     if (initial > current){
@@ -11,21 +22,26 @@ function calculateProfitAndLoss (initial, quantity, current) {
         var loss = (initial - current) * quantity;
         var lossPercentage = (loss/initial) * 100;
 
-       console.log(`Hey your loss is ${loss} and the percentage is ${lossPercentage}%`);
+       showOutput(`Hey your loss is ${loss} and the percentage is ${lossPercentage}%`);
 
     } else if (current > initial){
 
         var profit = (current-initial)*quantity;
         var profitPercentage = (profit/initial)*100;
 
-        console.log(`Hey your profit is ${profit} and your percentage is ${profitPercentage}%`);
+        showOutput(`Hey your profit is ${profit} and your percentage is ${profitPercentage}%`);
 
     } else {
 
-        console.log(`No pain no gain`);
+        showOutput(`No pain no gain`);
 
     }
 
 }
 
-calculateProfitAndLoss(100,10,100);
+calculateProfitAndLoss(10,10,100);
+
+function showOutput(message) {
+
+    outputBox.innerHTML = message;
+}
